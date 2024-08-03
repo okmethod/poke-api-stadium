2f3abdcce699b3a5b9bf2039ca6eee4836defb57
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-03 18:56:19 +0900

    :+1: build 404.html

--- skeleton-app/vite.config.ts
+++ skeleton-app/vite.config.ts
@@ -1,11 +1,44 @@
 import { purgeCss } from "vite-plugin-tailwind-purgecss";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import fs from "fs";
+import path from "path";
 
 const githubRepo = "poke-api-stadium";
 
+const content404 = ((base: string) => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Page Not Found</title>
+  <meta http-equiv="refresh" content="0; url=/#/">
+  <script>
+    // Redirect to the root URL
+    var path = window.location.pathname;
+    var newUrl = window.location.origin + '/${base}/';
+    window.location.replace(newUrl);
+  </script>
+</head>
+<body>
+  <p>Redirecting...</p>
+</body>
+</html>
+`)(githubRepo);
+
 export default defineConfig({
-  plugins: [sveltekit(), purgeCss()],
+  plugins: [
+    sveltekit(),
+    purgeCss(),
+    {
+      name: "generate-404",
+      closeBundle() {
+        fs.writeFileSync(path.resolve(__dirname, "build", "404.html"), content404);
+        console.log("404.html generated");
+      },
+    },
+  ],
   // Github Pagesで公開する場合は、base にリポジトリ名を指定
   base: `/${githubRepo}/`,
 });
