7459daec478bca1109690c099172667058282516
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-03 14:12:25 +0900

    :+1: put out component PokeCard

---
+++ skeleton-app/src/lib/components/PokeCard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { PokeData } from "$lib/types/poke";
+
+  export let pokeData: PokeData | null = null;
+</script>
+
+<div class="max-w-md mx-auto bg-white shadow-md rounded-lg overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="p-4">
+      <h1 class="text-2xl font-bold text-gray-900">{pokeData?.jaName} : {pokeData?.enName}</h1>
+      <img src={pokeData?.imageUrl} alt={pokeData?.jaName} class="w-48 h-48 mx-auto my-4" />
+
+      <h2 class="text-xl font-semibold text-gray-700 mt-4">タイプ</h2>
+      <ul class="list-decimal list-inside">
+        <li class="text-gray-600">{pokeData?.type1Name}</li>
+        <li class="text-gray-600">{pokeData?.type2Name !== null ? pokeData?.type2Name : "なし"}</li>
+      </ul>
+
+      <h2 class="text-xl font-semibold text-gray-700 mt-4">たかさ</h2>
+      <p class="text-gray-600">{(pokeData?.height * 0.1).toFixed(1)} m</p>
+
+      <h2 class="text-xl font-semibold text-gray-700 mt-4">おもさ</h2>
+      <p class="text-gray-600">{(pokeData?.weight * 0.1).toFixed(1)} kg</p>
+    </div>
+  </div>
+</div>
--- skeleton-app/src/routes/search/+page.svelte
+++ skeleton-app/src/routes/search/+page.svelte
@@ -2,16 +2,15 @@
   //import { fetchImageUrlToBlob } from "$lib/utils/request.client";
   import getPokeData from "$lib/api/getPokeData.client";
   import type { PokeData } from "$lib/types/poke";
+  import PokeCard from "$lib/components/PokeCard.svelte";
 
   let pokeId = "";
   let pokeData: PokeData | null = null;
-  let error: string | null = null;
 
   async function fetchPokeData() {
     try {
-      error = null;
       pokeData = await getPokeData(fetch, pokeId);
-    } catch (err) {
+    } catch {
       pokeData = null;
     }
   }
@@ -26,31 +25,6 @@
       <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">検索</button>
     </form>
 
-    {#if error}
-      <p class="text-red-500">エラー: {error}</p>
-    {/if}
-
-    {#if pokeData}
-      <div class="max-w-md mx-auto bg-white shadow-md rounded-lg overflow-hidden md:max-w-2xl">
-        <div class="md:flex">
-          <div class="p-4">
-            <h1 class="text-2xl font-bold text-gray-900">{pokeData.jaName} : {pokeData.enName}</h1>
-            <img src={pokeData.imageUrl} alt={pokeData.jaName} class="w-48 h-48 mx-auto my-4" />
-
-            <h2 class="text-xl font-semibold text-gray-700 mt-4">タイプ</h2>
-            <ul class="list-decimal list-inside">
-              <li class="text-gray-600">{pokeData.type1Name}</li>
-              <li class="text-gray-600">{pokeData.type2Name !== null ? pokeData.type2Name : "なし"}</li>
-            </ul>
-
-            <h2 class="text-xl font-semibold text-gray-700 mt-4">たかさ</h2>
-            <p class="text-gray-600">{(pokeData.height * 0.1).toFixed(1)} m</p>
-
-            <h2 class="text-xl font-semibold text-gray-700 mt-4">おもさ</h2>
-            <p class="text-gray-600">{(pokeData.weight * 0.1).toFixed(1)} kg</p>
-          </div>
-        </div>
-      </div>
-    {/if}
+    <PokeCard {pokeData} />
   </div>
 </div>
