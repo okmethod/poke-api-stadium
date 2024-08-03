7d1fac4b6f69db6c23f91f83512ce3596a9ad4e1
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-03 22:40:59 +0900

    :+1: use balls images

---
+++ skeleton-app/src/lib/types/ball.ts
@@ -0,0 +1,15 @@
+export interface Ball {
+  id: number;
+  imageUrl: string;
+}
+
+export const BALLS: { [name: string]: Ball } = {
+  "poke-ball": {
+    id: 4,
+    imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png",
+  },
+  "great-ball": {
+    id: 3,
+    imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/great-ball.png",
+  },
+};
--- skeleton-app/src/routes/(root)/+page.svelte
+++ skeleton-app/src/routes/(root)/+page.svelte
@@ -1,15 +1,26 @@
 <script>
   import { navigateTo } from "$lib/utils/navigation.client";
+  import { BALLS } from "$lib/types/ball";
 </script>
 
 <div class="h-full w-full mx-auto container">
-  <div class="space-y-6">
+  <div class="mb-6">
     <h1 class="text-3xl font-bold">PokeAPI スタジアムへようこそ !</h1>
+  </div>
+  <div class="flex flex-col space-y-2 items-start">
     <button
       on:click|preventDefault={() => navigateTo("/search")}
-      class="text-2xl text-blue-500 hover:underline font-bold py-2 px-4"
+      class="text-2xl text-blue-500 hover:underline font-bold py-2 px-4 flex items-center"
     >
+      <img src={BALLS["poke-ball"].imageUrl} alt="poke-ball" class="w-6 h-6 mr-2" />
       ポケモン検索
+    </button>
+    <button
+      on:click|preventDefault={() => navigateTo("/comapre")}
+      class="text-2xl text-blue-500 hover:underline font-bold py-2 px-4 flex items-center"
+    >
+      <img src={BALLS["great-ball"].imageUrl} alt="great-ball" class="w-6 h-6 mr-2" />
+      ポケモン XX くらべ
     </button>
   </div>
 </div>
