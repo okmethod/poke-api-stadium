a8593d791b7ea94a2397cccdd723f8bfca295f72
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-04 12:07:15 +0900

    :+1: show compare value

--- skeleton-app/src/routes/compare/+page.svelte
+++ skeleton-app/src/routes/compare/+page.svelte
@@ -5,17 +5,23 @@
   import type { PokeData } from "$lib/types/poke";
   import { LATEST_POKEMON_ID } from "$lib/types/poke";
   import PokeCardCompact from "$lib/components/PokeCardCompact.svelte";
-  import { getRandomNumbers } from "$lib/utils/numerics";
+  import { getRandomNumbers, formatHW } from "$lib/utils/numerics";
 
-  let paramId = "";
-  const params = {
-    height: "たかさ",
-    weight: "おもさ",
+  let paramId = "height";
+  interface Param {
+    name: string;
+    calcValue: (value: PokeData) => string;
+  }
+  const params: { [key: string]: Param } = {
+    height: { name: "たかさ", calcValue: (value: PokeData) => formatHW(value.height, "height") },
+    weight: { name: "おもさ", calcValue: (value: PokeData) => formatHW(value.weight, "weight") },
   };
 
+  let isOpen = false;
   let pokeIds: number[] = [];
   let pokeDataArray: PokeData[] = [];
   async function fetchPokeDataArray() {
+    isOpen = false;
     try {
       pokeIds = getRandomNumbers(1, LATEST_POKEMON_ID, 3);
       pokeDataArray = await Promise.all(pokeIds.slice(0, 3).map((id) => getPokeData(fetch, id.toString())));
@@ -38,27 +44,29 @@
   const dropTargetStyle = { outline: "0px" };
 </script>
 
-<div class="container mx-auto h-full w-full">
+<div class="container mx-auto h-full w-9/12 ml-4">
   <div class="mb-2">
     <h1 class="text-2xl font-bold">ポケモン XX くらべ</h1>
   </div>
   <div class="space-y-5 min-w-[300px] max-w-[1200px]">
-    <form on:submit|preventDefault={fetchPokeDataArray} class="flex items-center space-x-3">
-      <select id="params" bind:value={paramId} class="border rounded px-12 py-1">
-        {#each Object.entries(params) as [key, value]}
-          <option value={key}>{value}</option>
-        {/each}
-      </select>
-      <div class="flex items-center space-x-2">
-        <label for="params" class="text-lg"> でくらべる </label>
-        <button
-          type="submit"
-          class="bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded h-full flex items-center"
-        >
-          <Icon icon="mdi:pokemon-go" class="w-5 h-5" />
-        </button>
-      </div>
-    </form>
+    <div class="ml-4">
+      <form on:submit|preventDefault={fetchPokeDataArray} class="flex items-center space-x-3">
+        <select id="params" bind:value={paramId} class="border rounded px-12 py-1">
+          {#each Object.entries(params) as [key, value]}
+            <option value={key}>{value.name}</option>
+          {/each}
+        </select>
+        <div class="flex items-center space-x-2">
+          <label for="params" class="text-lg"> でくらべる </label>
+          <button
+            type="submit"
+            class="bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded h-full flex items-center"
+          >
+            <Icon icon="mdi:pokemon-go" class="w-5 h-5" />
+          </button>
+        </div>
+      </form>
+    </div>
 
     <div class="space-y-5 border bg-white rounded-xl min-h-[300px] min-w-[300px]">
       <div
@@ -70,10 +78,19 @@
         {#each pokeDataArray as pokeData (pokeData.id)}
           <div>
             <PokeCardCompact {pokeData} />
-            <p class="text-center">hoge</p>
+            <p class="text-center">{isOpen ? params[paramId].calcValue(pokeData) : "???"}</p>
           </div>
         {/each}
       </div>
+    </div>
+    <div class="ml-4">
+      <button
+        type="button"
+        class="bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded h-full flex items-center"
+        on:click={() => (isOpen = !isOpen)}
+      >
+        <Icon icon="mdi:eye" class="w-5 h-5" />
+      </button>
     </div>
   </div>
 </div>
