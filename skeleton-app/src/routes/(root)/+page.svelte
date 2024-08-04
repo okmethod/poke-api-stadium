ca32b4b8f18693b8873baf44cc6da23489afbb2c
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-04 14:25:50 +0900

    :recycle: each buttons for contents link

--- skeleton-app/src/routes/(root)/+page.svelte
+++ skeleton-app/src/routes/(root)/+page.svelte
@@ -3,6 +3,33 @@
   import { BALLS } from "$lib/types/ball";
 
   const githubRepoUrl = "https://github.com/okmethod/poke-api-stadium";
+
+  const contents = [
+    {
+      text: "ポケモンずかん",
+      imageUrl: BALLS["poke-ball"].imageUrl,
+      alt: "poke-ball",
+      onClick: () => navigateTo("/search"),
+    },
+    {
+      text: "ポケモンXXくらべ",
+      imageUrl: BALLS["great-ball"].imageUrl,
+      alt: "great-ball",
+      onClick: () => navigateTo("/compare"),
+    },
+    {
+      text: "ポケモンタイプじゃんけん (制作中)",
+      imageUrl: BALLS["ultra-ball"].imageUrl,
+      alt: "great-ball",
+      onClick: () => navigateTo("/janken"),
+    },
+    {
+      text: "ソースコード",
+      imageUrl: BALLS["premier-ball"].imageUrl,
+      alt: "premier-ball",
+      onClick: () => window.open(githubRepoUrl, "_blank"),
+    },
+  ];
 </script>
 
 <div class="container mx-auto h-full w-9/12 ml-4">
@@ -10,33 +37,14 @@
     <h1 class="text-3xl font-bold">PokeAPI スタジアムへようこそ !</h1>
   </div>
   <div class="flex flex-col space-y-2 items-start">
-    <button
-      on:click|preventDefault={() => navigateTo("/search")}
-      class="text-2xl text-blue-500 hover:underline font-bold py-2 px-4 flex items-center"
-    >
-      <img src={BALLS["poke-ball"].imageUrl} alt="poke-ball" class="w-6 h-6 mr-2" />
-      ポケモンずかん
-    </button>
-    <button
-      on:click|preventDefault={() => navigateTo("/compare")}
-      class="text-2xl text-blue-500 hover:underline font-bold py-2 px-4 flex items-center"
-    >
-      <img src={BALLS["great-ball"].imageUrl} alt="great-ball" class="w-6 h-6 mr-2" />
-      ポケモンXXくらべ
-    </button>
-    <button
-      on:click|preventDefault={() => navigateTo("/janken")}
-      class="text-2xl text-blue-500 hover:underline font-bold py-2 px-4 flex items-center"
-    >
-      <img src={BALLS["ultra-ball"].imageUrl} alt="great-ball" class="w-6 h-6 mr-2" />
-      ポケモンタイプじゃんけん (制作中)
-    </button>
-    <button
-      on:click|preventDefault={() => window.open(githubRepoUrl, "_blank")}
-      class="text-2xl text-blue-500 hover:underline font-bold py-2 px-4 flex items-center"
-    >
-      <img src={BALLS["premier-ball"].imageUrl} alt="premier-ball" class="w-6 h-6 mr-2" />
-      ソースコード
-    </button>
+    {#each contents as item}
+      <button
+        on:click|preventDefault={item.onClick}
+        class="text-2xl text-blue-500 hover:underline font-bold py-2 px-4 flex items-center"
+      >
+        <img src={item.imageUrl} alt={item.alt} class="w-6 h-6 mr-2" />
+        {item.text}
+      </button>
+    {/each}
   </div>
 </div>
