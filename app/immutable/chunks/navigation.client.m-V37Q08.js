aa3550877c37ff4534c7191c27ece69b456ad24e
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-17 13:42:46 +0900

    Updates

---
+++ app/immutable/chunks/navigation.client.m-V37Q08.js
@@ -0,0 +1,1 @@
+import{g as a}from"./entry.BjioM_M8.js";import{b as t}from"./paths.C5fi057k.js";function m(o){a(`${t}${o}`)}export{m as n};
--- app/immutable/entry/start._Uv7chmT.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.uh2Dghm1.js";export{t as start};
