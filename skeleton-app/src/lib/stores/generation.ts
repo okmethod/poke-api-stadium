c4abc8c6a20c1c78d9ae7e223df8471523bada48
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-18 13:57:54 +0900

    :+1: add generation select in header layout

---
+++ skeleton-app/src/lib/stores/generation.ts
@@ -0,0 +1,56 @@
+import { writable } from "svelte/store";
+
+export type GenerationId = "all" | "gen1" | "gen2" | "gen3" | "gen4" | "gen5" | "gen6" | "gen7" | "gen8" | "gen9";
+
+export interface GenerationData {
+  label: string;
+  description: string;
+}
+
+const savedGenerationId =
+  typeof localStorage !== "undefined" ? (localStorage.getItem("generationId") as GenerationId) : "all";
+
+export const generationId = writable<GenerationId>(savedGenerationId ?? "all");
+
+export const generations: Record<GenerationId, GenerationData> = {
+  gen1: {
+    label: "第1世代",
+    description: "赤・緑・青・黄",
+  },
+  gen2: {
+    label: "第2世代",
+    description: "金・銀・クリスタル",
+  },
+  gen3: {
+    label: "第3世代",
+    description: "ルビー・サファイア・エメラルド",
+  },
+  gen4: {
+    label: "第4世代",
+    description: "ダイヤモンド・パール",
+  },
+  gen5: {
+    label: "第5世代",
+    description: "ブラック・ホワイト",
+  },
+  gen6: {
+    label: "第6世代",
+    description: "X・Y",
+  },
+  gen7: {
+    label: "第7世代",
+    description: "サン・ムーン",
+  },
+  gen8: {
+    label: "第8世代",
+    description: "ソード・シールド",
+  },
+  gen9: {
+    label: "第9世代",
+    description: "スカーレット・バイオレット",
+  },
+  all: {
+    label: "全世代",
+    description: "すべて",
+  },
+};
--- skeleton-app/src/routes/+layout.svelte
+++ skeleton-app/src/routes/+layout.svelte
@@ -1,15 +1,40 @@
 <script lang="ts">
   import "../app.postcss";
-  import { AppBar } from "@skeletonlabs/skeleton";
-  import { Toast, Modal, initializeStores } from "@skeletonlabs/skeleton";
-  initializeStores();
-
-  // Floating UI for Popups
+  import { onMount } from "svelte";
+  import { Toast, Modal, initializeStores, storePopup } from "@skeletonlabs/skeleton";
+  import Icon from "@iconify/svelte";
   import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom";
-  import { storePopup } from "@skeletonlabs/skeleton";
+  import { generations, generationId, type GenerationId } from "$lib/stores/generation.js";
+  import { navigateTo } from "$lib/utils/navigation.client";
+
+  initializeStores();
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
-  import { navigateTo } from "$lib/utils/navigation.client";
+  let options = [
+    { value: "", label: "" },
+    ...Object.entries(generations).map(([value, { label }]) => ({ value, label })),
+  ];
+
+  let currentGenerationId: GenerationId | null = null;
+  onMount(() => {
+    if (typeof localStorage !== "undefined") {
+      const savedGenerationId = localStorage.getItem("generationId") as GenerationId;
+      if (savedGenerationId) generationId.set(savedGenerationId);
+      options = options.filter((option) => option.value !== "");
+    }
+  });
+
+  generationId.subscribe((value: GenerationId) => {
+    currentGenerationId = value;
+    if (typeof localStorage !== "undefined") {
+      localStorage.setItem("generationId", value);
+    }
+  });
+
+  function handleChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    generationId.set(target.value as GenerationId);
+  }
 </script>
 
 <svelte:head>
@@ -21,14 +46,28 @@
 <Toast position="tr" rounded="rounded-lg" />
 
 <div class="flex flex-col h-screen">
-  <div class="border-b border-gray-400">
-    <AppBar class="!p-1 md:!p-2">
-      <div class="flex items-center h-full">
-        <a href="/" class="flex items-center" on:click|preventDefault={() => navigateTo("/")}>
-          <div class="text-sm md:text-lg">TOP</div>
-        </a>
-      </div>
-    </AppBar>
+  <div class="border-b border-gray-400 bg-gray-100">
+    <div class="flex items-center justify-between h-full">
+      <a
+        href="/"
+        class="flex flex-row items-center space-x-1 pt-1 pb-1 pl-1 pr-2 m-1 text-sm text-gray-500 bg-white border border-gray-400 rounded-md"
+        on:click|preventDefault={() => navigateTo("/")}
+      >
+        <div class="w-5 h-5">
+          <Icon icon="mdi:home-outline" class="text-gray-500 w-full h-full" />
+        </div>
+        <span class="">HOME</span>
+      </a>
+      <select
+        class="w-24 pt-1 pb-1 pl-2 pr-2 m-1 text-sm text-gray-500 border-gray-400 rounded-md"
+        bind:value={currentGenerationId}
+        on:change={handleChange}
+      >
+        {#each options as option}
+          <option value={option.value}>{option.label}</option>
+        {/each}
+      </select>
+    </div>
   </div>
 
   <div class="container mx-auto overflow-y-auto pb-16">
