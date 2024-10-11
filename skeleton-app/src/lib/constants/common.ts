db92f1a51a3f12d56d09bf2bf580606986c6affc
shohei.okugi <shohei.okugi@ghelia.com>
2024-10-11 22:07:50 +0900

    :+1: update MASTER_DRILL_URL

--- skeleton-app/src/lib/constants/common.ts
+++ skeleton-app/src/lib/constants/common.ts
@@ -5,9 +5,9 @@ const GITHUB_URL = "https://okmethod.github.io";
 export const MATTER_PROTOTYPE_URL = `${GITHUB_URL}/thin-2d-physics-sim`;
 export const AMMO_PROTOTYPE_URL = `${GITHUB_URL}/thin-3d-physics-sim`;
 export const PGM_BOY_ADVANCE_URL = `${GITHUB_URL}/programming-boy-advance`;
-export const MASTER_DRILL_URL = `${GITHUB_URL}/master-drill`;
 
 export const POKENATOR_URL = "https://okmethod-gemini-trial.web.app";
+export const MASTER_DRILL_URL = "https://okmethod-master-drill.web.app";
 
 // 通常フォルムのポケモンの図鑑番号の最小値〜最大値
 export const FIRST_POKE_ID = 1;
