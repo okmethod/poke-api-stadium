6a3815691a28da9a5c338e4d4a2cd4cbe5cc9350
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-09 18:51:11 +0900

    :+1: adjust root page style

--- skeleton-app/src/routes/(root)/+page.svelte
+++ skeleton-app/src/routes/(root)/+page.svelte
@@ -7,25 +7,23 @@
 </script>
 
 <div class="cRouteBodyStyle !relative">
-  <!-- タイトル部 -->
-  <header class="sticky absolute top-0 flex justify-center w-full bg-white z-10">
-    <div class="cTitlePartStyle !m-0 !mt-2 !mb-2 md:!mt-4 md:!mb-4">
-      <h1 class="cTitleStyle md:!text-3xl">
-        <span class="block xs:inline">PokeAPI スタジアム</span>
-        <span class="block text-right xs:inline">へようこそ !</span>
-      </h1>
-    </div>
-  </header>
+  <div class="cTitlePartStyle !m-0 !mt-2 !mb-2 md:!mt-4 md:!mb-4">
+    <h1 class="cTitleStyle md:!text-3xl">
+      <span class="block xs:inline">PokeAPI スタジアム</span>
+      <span class="block text-right xs:inline">へようこそ !</span>
+    </h1>
+  </div>
 
-  <!-- コンテンツ部 -->
   <div class="cContentPartStyle !mt-4 !relative">
-    {#each data.buttonConfigs as config}
-      <div class="">
-        <button on:click|preventDefault={config.onClick} class="flex items-center">
-          <img src={config.imageUrl} alt={config.alt} class="w-6 h-6 mr-2" />
-          <span class="cLinkButtonStyle md:!text-2xl">{config.title}</span>
-        </button>
-      </div>
-    {/each}
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+      {#each data.buttonConfigs as config}
+        <div class="">
+          <button type="button" class="btn variant-ghost flex items-center" on:click={config.onClick}>
+            <img src={config.imageUrl} alt={config.alt} class="w-6 h-6 mr-2" />
+            <span class="hover:underline text-xl md:text-2xl w-80">{config.title}</span>
+          </button>
+        </div>
+      {/each}
+    </div>
   </div>
 </div>
