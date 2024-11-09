a4c23ea762b6168b6f0205f4ff86c0b12b1c476e
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-10 00:00:33 +0900

    Updates

---
+++ app/immutable/chunks/navigation.client.D-68iLqd.js
@@ -0,0 +1,1 @@
+import{g as o}from"./entry.Beeiaigj.js";import{b as r}from"./paths.CX3vS59_.js";function s(s){o(`${r}${s}`)}export{s as n};
---
+++ app/immutable/chunks/paths.CX3vS59_.js
@@ -0,0 +1,1 @@
+var s,a;const l=(null==(s=globalThis.__sveltekit_1rogbgd)?void 0:s.base)??"/poke-api-stadium",o=(null==(a=globalThis.__sveltekit_1rogbgd)?void 0:a.assets)??l;export{o as a,l as b};
