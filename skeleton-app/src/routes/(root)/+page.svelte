b83417b89a2679874d7066f3bba6a53d29717fb5
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-09 20:35:48 +0900

    :+1: adjust root page

--- skeleton-app/src/routes/(root)/+page.svelte
+++ skeleton-app/src/routes/(root)/+page.svelte
@@ -6,7 +6,7 @@
   };
 </script>
 
-<div class="cRouteBodyStyle !relative">
+<div class="cRouteBodyStyle">
   <div class="cTitlePartStyle !m-0 !mt-2 !mb-2 md:!mt-4 md:!mb-4">
     <h1 class="cTitleStyle md:!text-3xl">
       <span class="block xs:inline">PokeAPI スタジアム</span>
@@ -14,21 +14,19 @@
     </h1>
   </div>
 
-  <div class="cContentPartStyle !mt-4 !relative">
+  <div class="cContentPartStyle !mt-4">
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
       {#each data.buttonConfigs as config}
-        <div class="">
-          <button type="button" class="btn variant-ghost flex items-center" on:click={config.onClick}>
-            <div class="w-6 h-6 mr-1">
-              {#if config.symbol === null}
-                <!-- no symbol -->
-              {:else if isImageConfig(config.symbol)}
-                <img src={config.symbol.src} alt={config.symbol.alt} class="w-full h-full" />
-              {/if}
-            </div>
-            <span class="hover:underline text-left text-xl md:text-2xl w-64 md:w-72">{config.label}</span>
-          </button>
-        </div>
+        <button type="button" class="btn variant-ghost flex items-center" on:click={config.onClick}>
+          <div class="w-6 h-6 mr-1">
+            {#if config.symbol === null}
+              <!-- no symbol -->
+            {:else if isImageConfig(config.symbol)}
+              <img src={config.symbol.src} alt={config.symbol.alt} class="w-full h-full" />
+            {/if}
+          </div>
+          <span class="hover:underline text-left text-xl md:text-2xl w-64 md:w-72">{config.label}</span>
+        </button>
       {/each}
     </div>
   </div>
